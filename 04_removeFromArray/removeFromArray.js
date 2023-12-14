27b0410013e65772c0a37850245a0e19bc30f0be
Tin Goricki <tingoricki1@protonmail.com>
2023-12-14 18:00:48 +0100

    Solved exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, inputVar1, inputVar2) {
+    for(let i = 0; i < array.length;i++) {
+        if(array[i] === inputVar1) {
+            array.splice(i,1)
+        }
+        if(array[i] === inputVar2) {
+            array.splice(i,1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
